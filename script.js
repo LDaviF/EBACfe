fe8bb49ca7cc91b1f7831647bc9fbd16203131da
Davi Ferreira <luisdaviferreira@gmail.com>
2024-03-25 21:28:08 -0300

    new visual element

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
+
+
+
 form.addEventListener('submit', function(event){
     event.preventDefault() // evita o envio do form
     
     let firstValue = parseInt(document.getElementById("valueA").value)
     let secondValue = parseInt(document.getElementById("valueB").value)
+    let feedback = document.getElementById("feedback")
+    
 
     
-    //firstValue = parseInt(firstValue)
-    //secondValue = parseInt(secondValue)
-    console.log(firstValue)
-    console.log(secondValue)
-    
-    if (secondValue > firstValue) {
-        alert("B é maior que A. Formulário válido")
-    } else if (firstValue == secondValue) {
-        alert("Valores iguais. Formulário inválido")
-    } else {
-        alert("A é maior que B. Formulário inválido")
+
+    if (secondValue > firstValue){  
+        feedback.innerHTML ="B is higher than A. Valid form"
+        feedback.style.background = "rgb(197, 232, 184)"
+        feedback.style.display = 'grid'
+    } else if (firstValue == secondValue){
+        feedback.innerHTML ="Equal values. Invalid form"
+        feedback.style.background = "rgb(248, 230, 230)"
+        feedback.style.display = 'grid'
+    } else if (firstValue > secondValue){
+        feedback.innerHTML ="A is higher than B. Invalid form"
+        feedback.style.background = "rgb(248, 230, 230)"
+        feedback.style.display = 'grid'   
     }
 })
